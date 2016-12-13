2ef9f0f6cd2da7d318ae4613e1341f1ba256ed93
Addy <addyleeliu@gmail.com>
2016-12-12 20:27:14 -0500

    fix clocking

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * *****
  * WRITTEN BY FLORIAN RAPPL, 2012.
  * florian-rappl.de
@@ -1418,6 +1418,7 @@ function chooseAvatar() {
 }
 
 function loadLevel() {
+<<<<<<< HEAD
 	var secondsLeft = 300;
 	var countDown = setInterval(function(){
 	  secondsLeft--;  
@@ -1427,14 +1428,22 @@ function loadLevel() {
       }
 	}, 1000); 
 	
+=======
+>>>>>>> parent of 85dd972... clock countdown
 	$("#game").empty().append(
 		$("<div>").attr('id', "world"))
 	.append(
 		$("<div>").attr('id', 'coinNumber').addClass("gauge").append("0"))
 	.append(
 		$("<div>").attr('id', 'coin').addClass("gaugeSprite"))
+	// .append(
+// 		$("<div>").attr('id', 'liveNumber').addClass("gauge").append("0"))
 	.append(
+<<<<<<< HEAD
 		$("<div>").attr('id', 'liveNumber').addClass("countdown").html(secondsLeft + "&nbsp" + "secs"))
+=======
+		$("<div>").attr('id', 'live').addClass("gaugeSprite"));
+>>>>>>> parent of 85dd972... clock countdown
 	var level = new Level('world');
 	level.load(definedLevels[0]);
 	level.start();
